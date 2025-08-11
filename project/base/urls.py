ef1b2e1d7d4b05c6414ab93b52a530535eae35c0
Ivan <saxareg2007@gmail.com>
2025-08-11 23:55:35 +0300

    Making project and some else

---
+++ project/base/urls.py
@@ -0,0 +1,18 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('shop/', include('shop.urls'))
+
+]
+
+# Добавляем маршруты для медиа-файлов (изображений, загружаемых пользователями)
+# только в режиме DEBUG (разработки)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
---
+++ project/shop/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
+    slug = models.CharField(max_length=255, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=300)
+    price = models.FloatField()
+    students_qty = models.IntegerField()
+    reviews_qty = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    slug = models.CharField(max_length=255, unique=True)
+    image = models.ImageField(upload_to='courses/images')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
