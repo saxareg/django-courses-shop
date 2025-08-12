fe1eaf60e96ea4e20715ce723e2c073b5469be82
Ivan <saxareg2007@gmail.com>
2025-08-12 20:05:11 +0300

    developing templates

--- project/shop/views.py
+++ project/shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Course
 
 
 def index(request):
-    return HttpResponse("Hello from the Shop app")
+    courses = Course.objects.all()
+    return render(request, 'courses.html', {'courses': courses})
