fc2d53ac5837f030f788d1ddc9d045954445a8b3
Ivan <saxareg2007@gmail.com>
2025-08-12 13:44:57 +0300

    automatization of slugs

--- project/shop/admin.py
+++ project/shop/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .import models
 
-admin.site.register(models.Category)
-admin.site.register(models.Course)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Course, ProductAdmin)
