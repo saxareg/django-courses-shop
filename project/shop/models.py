fbe2504c8e80f49043bb6b895a8621534071e747
Ivan <saxareg2007@gmail.com>
2025-08-16 23:32:01 +0300

    Adding some comments

--- project/shop/models.py
+++ project/shop/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
 
+# category class
+
 
 class Category(models.Model):
     title = models.CharField(max_length=255)
