1ef187552f519eea8caac343f33ef5588e2d3cc6
Ivan <saxareg2007@gmail.com>
2025-08-17 23:56:35 +0300

    Commenting else

--- project/shop/views.py
+++ project/shop/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Course, Category
 
-
+# old method
 # def index(request):
 #     courses = Course.objects.all()
 #     return render(request, 'shop/courses.html', {'courses': courses})
+
 
 def index(request):
     courses = Course.objects.all().order_by('-students_qty')
