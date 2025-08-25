795f6b7ba18977c57d7e3e8b62229df2a54e9723
Ivan <saxareg2007@gmail.com>
2025-08-25 21:07:30 +0300

    Changing settings

--- project/base/settings.py
+++ project/base/settings.py
@@ -128,3 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')    # для production
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_URL = 'users:login'
