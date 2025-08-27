153d138b9c603a63f2cf54f0066fd96e5c73bce5
Ivan <saxareg2007@gmail.com>
2025-08-27 15:03:24 +0300

    Writing some path

--- project/shop/urls.py
+++ project/shop/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 app_name = 'shop'
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('filter/', views.filter_courses, name='filter'),
     path('<str:my_slug>', views.single_course, name='single_course'),
+    path('buy/<int:course_id>/', views.buy_course, name='buy_course'),
+    path('my-courses/', views.my_courses, name='my_courses'),
 ]
